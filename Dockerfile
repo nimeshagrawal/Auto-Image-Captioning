3d272ed9d3bfe11188361ee95ef95d488e73e5c3
Nimesh Agrawal <nimeshagrawal84@gmail.com>
2023-08-15 01:13:16 +0530

    updates

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM python:3.7-buster 
+FROM python:3.7-buster
 COPY . /app             
 WORKDIR /app
-RUN pip install -r requirements.txt
+RUN pip install -r requirements.txt &&\
+    ls -la /app/uploads &&\
+    chmod 777 /app/uploads/
 EXPOSE 7860
 CMD ["python3","-m", "flask", "run","--host","0.0.0.0","--port","7860"]
--- app.py
+++ app.py
@@ -1,108 +1,84 @@
-from __future__ import division, print_function
-# coding=utf-8
-import sys
-import os
-import glob
-import re
-import numpy as np
+#Libraries
 
-# Keras
-import tensorflow as tf
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+import pickle
+import numpy as np
+import os
+from tqdm import tqdm
+import json
+import keras
+
+#from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+#from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.applications.mobilenet import MobileNet, preprocess_input
+from tensorflow.keras.utils import to_categorical, plot_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Input, Dense, LSTM, Embedding, Dropout, add
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+
+from PIL import Image
+import matplotlib.pyplot as plt
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 #######################################################################
-#from tkinter import *
-#from tkinter.ttk import *
-from PIL import Image, ImageTk, ImageOps
-#from tkinter.filedialog import askopenfilename 
-from keras.models import load_model
-import keras
-import numpy as np
-import pandas as pd
-# import cv2
-import os
-from werkzeug.utils import secure_filename
 
-
-#from tensorflow import keras 
-from PIL import Image, ImageOps
-# import cv2 
-import random
-import numpy as np
-import os
-import pandas as pd
-from matplotlib import pyplot as plt
-
-from keras.callbacks import ModelCheckpoint, EarlyStopping
-import time
-from tqdm import tqdm
-from tensorflow.keras.models import load_model
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
-
-########################################################################
-
-
-classes = { 1:'Speed limit (20km/h)',
- 			2:'Speed limit (30km/h)', 
- 			3:'Speed limit (50km/h)', 
- 			4:'Speed limit (60km/h)', 
- 			5:'Speed limit (70km/h)', 
- 			6:'Speed limit (80km/h)', 
- 			7:'End of speed limit (80km/h)', 
- 			8:'Speed limit (100km/h)', 
- 			9:'Speed limit (120km/h)', 
- 			10:'No passing', 
- 			11:'No passing veh over 3.5 tons', 
- 			12:'Right-of-way at intersection', 
- 			13:'Priority road', 
- 			14:'Yield', 
- 			15:'Stop', 
- 			16:'No vehicles', 
- 			17:'Veh > 3.5 tons prohibited', 
- 			18:'No entry', 
- 			19:'General caution', 
- 			20:'Dangerous curve left', 
- 			21:'Dangerous curve right', 
- 			22:'Double curve', 
- 			23:'Bumpy road', 
- 			24:'Slippery road', 
- 			25:'Road narrows on the right', 
- 			26:'Road work', 
- 			27:'Traffic signals', 
- 			28:'Pedestrians', 
-			29:'Children crossing', 
- 			30:'Bicycles crossing', 
- 			31:'Beware of ice/snow',
- 			32:'Wild animals crossing', 
- 			33:'End speed + passing limits', 
- 			34:'Turn right ahead', 
- 			35:'Turn left ahead', 
- 			36:'Ahead only', 
- 			37:'Go straight or right', 
- 			38:'Go straight or left', 
- 			39:'Keep right', 
- 			40:'Keep left', 
- 			41:'Roundabout mandatory', 
- 			42:'End of no passing', 
- 			43:'End no passing veh > 3.5 tons' }
 
 # Define a flask app
 app = Flask(__name__)
 
+# load features form pickle
+with open(os.path.join(os.getcwd(), 'features2.pkl'), "rb") as f:
+  features = pickle.load(f)
+
+# load features form pickle
+with open(os.path.join(os.getcwd(), 'tokenizer2.pkl'), "rb") as f:
+  tokenizer = pickle.load(f)
+
+
+# Opening JSON file
+with open('meta_data_mobilenet.json', 'r') as openfile:
+    # Reading from json file
+    json_object = json.load(openfile)
+ 
+max_length, vocab_size = json_object['max_length'], json_object['vocab_size']
+
+def get_model():
+    inputs1 = Input(shape=(1000,))
+    fe1 = Dropout(0.4)(inputs1)
+    fe2 = Dense(256, activation='relu')(fe1)
+    # sequence feature layers
+    inputs2 = Input(shape=(max_length,))
+    se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
+    #se1 = Word2Vec(all_captions, 256, min_count=1)(inputs2)
+    se2 = Dropout(0.4)(se1)
+    se3 = LSTM(256, return_sequences=True)(se2)
+    se4 = LSTM(256)(se3)
+
+    # decoder model
+    decoder1 = add([fe2, se4])
+    decoder2 = Dense(256, activation='relu')(decoder1)
+    outputs = Dense(vocab_size, activation='softmax')(decoder2)
+
+    model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+    opt = Adam()
+    model.compile(loss='categorical_crossentropy', optimizer=opt)
+
+    return model
+
+model = get_model()
+
+
 # Model saved with Keras model.save()
-MODEL_PATH = os.path.join(os.getcwd(), "traffic_sign_model_grayscale_2.model")
+MODEL_PATH = os.path.join(os.getcwd(), "model_mobilenet.h5")
 
 # Load your trained model
-model = load_model(MODEL_PATH)
+model.load_weights(MODEL_PATH)
 #model._make_predict_function()          # Necessary
 print('Model loaded. Start serving...')
 
@@ -114,47 +90,50 @@ print('Model loaded. Start serving...')
 print('Model loaded......... Check http://127.0.0.1:5000/')
 
 
-def model_predict(model,img_path):
-    # message= request.get_json(force=True)
-    # encoded=message['image']
-    # decoded=base64.b64decode(encoded)
-    img = Image.open(img_path)
-    img = ImageOps.grayscale(img)
-    
-    image_new=img.resize((30,30))
-    # Model saved with Keras model.save()
-    #MODEL_PATH = 'models/traffic_sign_model_grayscale_2.model'
+def idx_to_word(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
 
-    # Load your trained model
-    #model = keras.models.load_model(MODEL_PATH)
-    #turn the image into a numpy array
-    x=np.array(image_new)
-    # x_gray = cv2.cvtColor(x, cv2.COLOR_RGB2GRAY)
-    
-    x_gray = np.array(x)
-    x_gray = x_gray.reshape(1,x_gray.shape[0],x_gray.shape[1],1)
-    print(x_gray.shape)
-    preds = model.predict(x_gray)
-    print('\n\n\npredicted successfully............\n\n\n')
-    
-    
-    # Preprocessing the image
-    #x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    #x = np.expand_dims(x, axis=0)
+def predict_caption(model, image, tokenizer, max_length):
+    # add start tag for generation process
+    in_text = 'startseq'
+    # iterate over the max length of sequence
+    for i in range(max_length):
+        # encode input sequence
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        # pad the sequence
+        sequence = pad_sequences([sequence], max_length)
+        # predict next word
+        yhat = model.predict([image, sequence], verbose=0)
+        # get index with high probability
+        yhat = np.argmax(yhat)
+        # convert index to word
+        word = idx_to_word(yhat, tokenizer)
+        # stop if word not found
+        if word is None:
+            break
+        # append word as input for generating next word
+        in_text += " " + word
+        # stop if we reach end tag
+        if word == 'endseq':
+            break
+      
+    return in_text
 
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    
-    #x = preprocess_input(x, mode='caffe')
-
-    #preds = model.predict(x)
-    preds = list(preds[0])
-            
-    pred_class_id = preds.index(max(preds)) +1
-    #print(preds)
-    #print(pred_class_id)
-    return pred_class_id
+def generate_caption(image_name, model):
+    # load the image
+    # image_name = "1001773457_577c3a7d70.jpg"
+    image_id = image_name.split('/')[-1][:-4]
+    img_path = image_name
+    image = Image.open(img_path)
+    # predict the caption
+    y_pred = predict_caption(model, features[image_id], tokenizer, max_length)
+    #print('--------------------Predicted--------------------')
+    #print(y_pred)
+    #plt.imshow(image)
+    return y_pred
 
 
 @app.route('/', methods=['GET'])
@@ -179,22 +158,13 @@ def upload():
         print(file_path)
         print("\n\n\n\n\n")
 
-        # Make prediction
-        #preds = model_predict(file_path, model)
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        #pred_class = preds.index(max(preds)) +1        # ImageNet Decode
-        cid = model_predict(model, file_path)
-        result = classes[cid]                    # Convert to string
-        
-        print("\n\nGot the result\n\n")     
-        print(result)
-        print("\n\n\n\n")
+       
+        y_pred = generate_caption(file_path, model)
 
-        return result   
+        return y_pred 
     return None
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=7860 )
+    app.run(debug=False, host='0.0.0.0', port=7860)
 
